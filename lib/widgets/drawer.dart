00f56c4618117fb730c598206535b39ada893f5e
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-02-09 23:38:45 +0530

    material drawer | dev tools | list view | netWork Image

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://pbs.twimg.com/profile_images/1349314829585039360/NuZlrNUQ_400x400.jpg";
+
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("Shashank Agarwal"),
+                accountEmail: Text("shashankagarwal.official@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.home, color: Colors.white),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading:
+                  Icon(CupertinoIcons.profile_circled, color: Colors.white),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(CupertinoIcons.mail, color: Colors.white),
+              title: Text(
+                "Email Me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
