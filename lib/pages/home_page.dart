6adf3048d33ee840e122010d96b6636d4fb53fe0
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-02-12 12:57:53 +0530

    day11 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
+// Day11 - We learn about the BuildContext, Constrains and 3 trees such as Widget, Element and Render Tree in Flutter
+
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Shashank Agarwal";
